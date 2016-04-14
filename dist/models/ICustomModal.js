de23c20ab91ae927af5291b5fc50a71cca54f406
Tigran Tokmajyan <toktikweb@gmail.com>
2016-04-14 18:17:42 +0400

    initial

---
+++ dist/models/ICustomModal.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * A Type used as a binding key for dialog window Components
+ */
+var ICustomModal = (function () {
+    function ICustomModal() {
+    }
+    return ICustomModal;
+}());
+exports.ICustomModal = ICustomModal;
+//# sourceMappingURL=ICustomModal.js.map
