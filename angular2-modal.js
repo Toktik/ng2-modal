610955e2d8df2f3776172e0bf4a95718b8191507
Tigran Tokmajyan <toktikweb@gmail.com>
2016-04-14 18:37:34 +0400

    fixes

---
+++ angular2-modal.js
@@ -0,0 +1,13 @@
+/**
+ * A Dummy file for the Webstrom IDE so it can access a directory as a package hence get intellisense.
+ * Since only a directory in "node_modeules" is auto imported by WebStorm, this is a workaround.
+ *
+ * THIS FILE IS NEVER LOADED BY WEBPACK AND DOES NOT GO INTO THE BUNDLE
+ *
+ * See open issue: https://youtrack.jetbrains.com/issue/WEB-17533
+ */
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require('./src/components/angular2-modal/index'));
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYW5ndWxhcjItbW9kYWwuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJhbmd1bGFyMi1tb2RhbC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTs7Ozs7OztHQU9HOzs7O0FBRUgsaUJBQWMsdUNBQXVDLENBQUMsRUFBQSIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxyXG4gKiBBIER1bW15IGZpbGUgZm9yIHRoZSBXZWJzdHJvbSBJREUgc28gaXQgY2FuIGFjY2VzcyBhIGRpcmVjdG9yeSBhcyBhIHBhY2thZ2UgaGVuY2UgZ2V0IGludGVsbGlzZW5zZS5cclxuICogU2luY2Ugb25seSBhIGRpcmVjdG9yeSBpbiBcIm5vZGVfbW9kZXVsZXNcIiBpcyBhdXRvIGltcG9ydGVkIGJ5IFdlYlN0b3JtLCB0aGlzIGlzIGEgd29ya2Fyb3VuZC5cclxuICpcclxuICogVEhJUyBGSUxFIElTIE5FVkVSIExPQURFRCBCWSBXRUJQQUNLIEFORCBET0VTIE5PVCBHTyBJTlRPIFRIRSBCVU5ETEVcclxuICpcclxuICogU2VlIG9wZW4gaXNzdWU6IGh0dHBzOi8veW91dHJhY2suamV0YnJhaW5zLmNvbS9pc3N1ZS9XRUItMTc1MzNcclxuICovXHJcblxyXG5leHBvcnQgKiBmcm9tICcuL3NyYy9jb21wb25lbnRzL2FuZ3VsYXIyLW1vZGFsL2luZGV4JztcclxuIl19
---
+++ src/components/angular2-modal/index.js
@@ -0,0 +1,6 @@
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+// WEBPACK development runtime helper.
+__export(require('./angular2-modal'));
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQSxzQ0FBc0M7QUFDdEMsaUJBQWMsa0JBQWtCLENBQUMsRUFBQSIsInNvdXJjZXNDb250ZW50IjpbIi8vIFdFQlBBQ0sgZGV2ZWxvcG1lbnQgcnVudGltZSBoZWxwZXIuXHJcbmV4cG9ydCAqIGZyb20gJy4vYW5ndWxhcjItbW9kYWwnOyJdfQ==
